389ba8efb52a14e0c8f8ddb9dcaef73c4ce690ea
Andrei <pantoja.andrei.d@gmail.com>
2026-04-15 12:50:57 +0800

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/ojt-tracker/",
+  base: '/ojt-tracker/',
 })
